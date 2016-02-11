bd8f3a495502cb4d2a8bc7b7b9bc9ae513e5f78e
Yang Bo <atryyang@thoughtworks.com>
2016-02-12 01:34:13 +0800

    Initial implementation

---
+++ Main.hx
@@ -0,0 +1,29 @@
+import com.thoughtworks.microbuilder.tutorial.githubSdk.model.*;
+import com.thoughtworks.microbuilder.tutorial.githubSdk.proxy.*;
+import com.thoughtworks.microbuilder.core.*;
+
+class Main {
+	
+	public static function main() {
+		// Initialize organization service in Github SDK
+		var organizationService = {
+			var urlPrefix = "https://api.github.com/";
+			var routeConfiguration = MicrobuilderRouteConfigurationFactory.routeConfiguration_com_thoughtworks_microbuilder_tutorial_githubSdk_rpc_IOrganizationService();
+			var outgoingJsonService = new MicrobuilderOutgoingJsonService(urlPrefix, routeConfiguration);
+			MicrobuilderOutgoingProxyFactory.outgoingProxy_com_thoughtworks_microbuilder_tutorial_githubSdk_rpc_IOrganizationService(outgoingJsonService);
+		}
+
+		// Read GITHUB_USERNAME environment variables
+		var username = Sys.environment()["GITHUB_USERNAME"];
+
+		// Send a request to Github API via organization service
+		organizationService.listUserOrganizations(username).start(
+			function(response):Void {
+				trace('$username belong to ${response.length} organizations: ${ [ for (organization in response) "\n * " + organization.login ].join("") }');
+			},
+			function(error):Void {
+				trace('Error: $error');
+			}
+		);
+	}
+}
